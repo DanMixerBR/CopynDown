1b726f53b5cab1d08c1d2b2ace0ec672e4fa2fe1
Danilo <danilo.social20@gmail.com>
2026-04-22 07:15:50 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -2,7 +2,7 @@
 
 powershell -command "Expand-Archive -Path 'CopynDown.zip' -DestinationPath '.' -Force"
 
-del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "Auto Update.bat" "CopynDown.exe.old" "CopynDown.zip" "update.bat" >nul 2>&1
+del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "Auto Update.bat" "CopynDown.exe.old" "CopynDown.zip" "update.bat" "bin\update.bat" >nul 2>&1
 
 echo.
 echo.
