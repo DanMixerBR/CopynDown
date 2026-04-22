fd25cfc18088294cbb5231691b55d993fdb3bd84
Danilo <danilo.social20@gmail.com>
2026-04-22 07:37:36 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,10 +1,11 @@
 @echo off
-
+echo.
+Updating CopynDown... Please wait.
+echo.
 powershell -command "Expand-Archive -Path 'CopynDown.zip' -DestinationPath '.' -Force"
 
-del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "Auto Update.bat" "CopynDown.exe.old" "CopynDown.zip" "update.bat" "bin\update.bat" >nul 2>&1
+del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "CopynDown.zip" "CopynDown.exe.old" >nul 2>&1
 
-echo.
 echo.
 echo.
 echo.
@@ -12,7 +13,8 @@ echo Update complete! Please restart the app.
 echo.
 echo.
 echo.
-echo.
-timeout /t 5
 
+del /f /q "update.bat" "bin\update.bat" "Auto Update.bat" >nul 2>&1
+
+timeout /t 5
 exit
