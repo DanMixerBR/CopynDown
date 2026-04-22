fa6b44a430598a27b7d191298caf5ad355dc7306
Danilo <danilo.social20@gmail.com>
2026-04-22 07:55:05 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,7 +1,8 @@
 @echo off
+title CopynDown Updater
 cls
 
-:: Cores ANSI (Definições)
+:: ANSI Color Definitions
 set "ESC="
 set "G=%ESC%[92m"
 set "C=%ESC%[96m"
@@ -9,23 +10,24 @@ set "W=%ESC%[0m"
 set "Y=%ESC%[93m"
 
 echo %C%=======================================================%W%
-echo           %G%CopynDown%W% - %Y%Gerenciador de Atualizacao%W%
+echo           %G%CopynDown%W% - %Y%Update Manager%W%
 echo %C%=======================================================%W%
 echo.
-echo %C%[%W%*%C%]%W% Status: %Y%Extraindo novos arquivos...%W%
+echo %C%[%W%*%C%]%W% Status: %Y%Extracting new files...%W%
 powershell -command "Expand-Archive -Path 'CopynDown.zip' -DestinationPath '.' -Force"
 
-echo %C%[%W%*%C%]%W% Status: %Y%Limpando arquivos obsoletos...%W%
+echo %C%[%W%*%C%]%W% Status: %Y%Cleaning up legacy files...%W%
 del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "CopynDown.zip" "CopynDown.exe.old" "bin\update.bat" "Auto Update.bat" >nul 2>&1
 
 echo.
 echo %G%-------------------------------------------------------%W%
-echo   [OK] Atualizacao concluida com sucesso!
-echo   Pressione qualquer tecla para reiniciar o app.
+echo   [SUCCESS] Update completed!
+echo   Please restart the app
 echo %G%-------------------------------------------------------%W%
 echo.
 
-pause >nul
-:: Comando para se auto-deletar e sair
+timeout /t 7 >nul
+
+:: Self-deletion logic to keep the workspace clean
 start /b "" cmd /c "timeout /t 1 >nul & del /f /q "%~f0""
 exit
