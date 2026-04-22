b6cc23ceef82970b20ba8a5e93f220a154ec3981
Danilo <danilo.social20@gmail.com>
2026-04-22 06:12:37 -0300

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,18 @@
+@echo off
+
+powershell -command "Expand-Archive -Path 'CopynDown.zip' -DestinationPath '.' -Force"
+
+del /f /q "YT Video Downloader.bat" "YT Music Downloader.bat" "YT MP3 Converter.bat" "Insta Video Downloader.bat" "Readme (EN).txt" "Readme (PT).txt" "YT.Video.Downloader.zip" "Auto Update.bat" "CopynDown.exe.old" "CopynDown.zip" >nul 2>&1
+
+echo.
+echo.
+echo.
+echo.
+echo Update complete! Please restart the app.
+echo.
+echo.
+echo.
+echo.
+timeout /t 5
+
+exit
