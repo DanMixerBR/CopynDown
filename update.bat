8865db1066e5c3340b109dd8a14dbb24b1d8fd08
Danilo <danilo.social20@gmail.com>
2026-04-25 22:58:39 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -14,8 +14,8 @@ echo           %G%CopynDown%W% - %Y%Update Manager%W%
 echo %C%=======================================================%W%
 echo.
 echo %C%[%W%*%C%]%W% Status: %Y%Extracting new files...%W%
-cd..
 powershell -command "Expand-Archive -Path 'CopynDown.zip' -DestinationPath '.' -Force" 2>&1
+cd..
 powershell -command "Expand-Archive -Path 'core\CopynDown.zip' -DestinationPath '.' -Force" 2>&1
 
 echo %C%[%W%*%C%]%W% Status: %Y%Cleaning up legacy files...%W%
