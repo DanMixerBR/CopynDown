f279c671c369670d2027814b6bf4ae0877ea7d32
Danilo <danilo.social20@gmail.com>
2026-04-25 22:41:53 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -15,7 +15,7 @@ echo %C%=======================================================%W%
 echo.
 echo %C%[%W%*%C%]%W% Status: %Y%Extracting new files...%W%
 cd..
-powershell -command "Expand-Archive -Path 'core\CopynDown.zip' -DestinationPath '.' -Force"
+powershell -command "Expand-Archive -Path 'core\CopynDown.zip' -DestinationPath 'core' -Force"
 
 echo %C%[%W%*%C%]%W% Status: %Y%Cleaning up legacy files...%W%
 rmdir /s /q "core\bin"
