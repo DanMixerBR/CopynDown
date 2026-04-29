96832dfecc41793bc7668cadcdeb13c1e3066e86
Danilo <danilo.social20@gmail.com>
2026-04-29 13:36:33 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ echo ""
 echo -e "${C}[${W}*${C}]${W} Status: ${Y}Extracting new files...${W}"
 
 # Extrai o zip sobrescrevendo os arquivos antigos sem perguntar (-o)
-unzip -o CopynDown_Linux.zip > /dev/null 2>&1
+unzip -o CopynDown_Linux.zip -d .. > /dev/null 2>&1
 
 # Deleta o arquivo zip após a extração
 rm -f CopynDown_Linux.zip
