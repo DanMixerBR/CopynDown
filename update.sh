dd4aa90624f372a3b10632a6f6370bae27367d2f
Danilo <danilo.social20@gmail.com>
2026-04-29 16:38:35 -0300

    Set execute permissions for CopynDown and binaries
    Add execute permissions to extracted binaries

--- update.sh
+++ update.sh
@@ -18,6 +18,12 @@ echo -e "${C}[${W}*${C}]${W} Status: ${Y}Extracting new files...${W}"
 # O PULO DO GATO: O "-d .." força a extração para uma pasta antes da atual
 unzip -o CopynDown_Linux.zip -d .. > /dev/null 2>&1
 
+chmod +x CopynDown.bin
+chmod +x bin/yt-dlp
+chmod +x bin/ffmpeg
+chmod +x bin/ffprobe
+chmod +x bin/deno
+
 # Deleta o arquivo zip (que continua na pasta atual) após a extração
 rm -f CopynDown_Linux.zip
 
